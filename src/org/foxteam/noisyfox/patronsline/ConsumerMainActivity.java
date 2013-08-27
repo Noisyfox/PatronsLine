686b3e4cd2e675e938f67f916705b58629a72886
noisyfox <timemanager.rick@gmail.com>
2013-08-28 00:06:54 +0800

    修正list item点击焦点错误的bug 增加基本的url请求函数

--- src/org/foxteam/noisyfox/patronsline/ConsumerMainActivity.java
+++ src/org/foxteam/noisyfox/patronsline/ConsumerMainActivity.java
@@ -1,16 +1,23 @@
 package org.foxteam.noisyfox.patronsline;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TabHost;
 import android.widget.TextView;
 import android.widget.ToggleButton;
@@ -47,6 +54,14 @@ public class ConsumerMainActivity extends SherlockFragmentActivity {
 				mTabHost.newTabSpec("favouriteFood").setIndicator(
 						getText(R.string.tab_label_favourite_food)),
 				FavouriteFoodFragment.class, null);
+		mTabsAdapter.addTab(
+				mTabHost.newTabSpec("favouriteFood2").setIndicator(
+						getText(R.string.tab_label_favourite_food)),
+				FavouriteFoodFragment.class, null);
+		mTabsAdapter.addTab(
+				mTabHost.newTabSpec("favouriteFood3").setIndicator(
+						getText(R.string.tab_label_favourite_food)),
+				FavouriteFoodFragment.class, null);
 
 		if (savedInstanceState != null) {
 			mTabHost.setCurrentTabByTag(savedInstanceState.getString("tab"));
@@ -69,31 +84,123 @@ public class ConsumerMainActivity extends SherlockFragmentActivity {
 	// -----------------------------------------------------------------------------------
 	public static class FavouriteFoodFragment extends SherlockListFragment {
 
+		List<InformationFood> mFoods = new ArrayList<InformationFood>();
+		FoodAdapter mFoodAdapter = null;
+		GetBookmarkTask mGetBookmarkTask = null;
+
 		@Override
 		public void onActivityCreated(Bundle savedInstanceState) {
 			super.onActivityCreated(savedInstanceState);
-			setHasOptionsMenu(true);
+			Log.d("msg", "onActivityCreated");
 
 			setEmptyText(getText(R.string.empty_text_no_favourite_food));
+			setHasOptionsMenu(true);
 
-			setListShown(false);
+			mFoodAdapter = new FoodAdapter(getActivity());
 
-			List<InformationFood> mFoods = new ArrayList<InformationFood>();
-			FoodAdapter mFoodAdapter = new FoodAdapter(mFoods);
+			setListAdapter(mFoodAdapter);
 
-			this.setListAdapter(mFoodAdapter);
+			InformationFood food = new InformationFood();
+			food.name = "鸭血粉丝汤";
+			food.id = "AAA";
+			food.bookmark = true;
+			food.price = 5.0f;
+			food.special = true;
+			food.photo = "AAAA";
+			food.photoBitmap = BitmapFactory.decodeResource(getResources(),
+					R.drawable.pic_food_example);
+			food.likes = 100;
+			food.dislikes = 0;
+			food.comments = 1;
+
+			mFoods.add(food);
+			mFoods.add(food);
+			mFoods.add(food);
+			mFoods.add(food);
+			mFoods.add(food);
+			mFoods.add(food);
+			mFoods.add(food);
+			mFoods.add(food);
+			mFoods.add(food);
+			mFoods.add(food);
+			mFoods.add(food);
+			mFoods.add(food);
+
+			refreshData();
 		}
+
+		MenuItem mItem_refresh = null;
 
 		@Override
 		public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-			MenuItem item = menu.add(R.string.menu_refresh);
-			item.setIcon(R.drawable.ic_menu_refresh);
-			item.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+			mItem_refresh = menu.add(R.string.menu_refresh);
+			mItem_refresh.setIcon(R.drawable.ic_menu_refresh);
+			mItem_refresh.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+		}
+
+		@Override
+		public boolean onOptionsItemSelected(MenuItem item) {
+			if (item == mItem_refresh) {
+				refreshData();
+				return true;
+			}
+
+			return super.onOptionsItemSelected(item);
+		}
+
+		@Override
+		public void onListItemClick(ListView l, View v, int position, long id) {
+			Log.d("msg", "onListItemClick");
+			super.onListItemClick(l, v, position, id);
+		}
+
+		private void refreshData() {
+			setListShown(false);
+
+			Log.d("msg", "update");
+
+			new GetBookmarkTask().execute("", "");
+		}
+
+		class GetBookmarkTask extends
+				AsyncTask<String, Void, List<InformationFood>> {
+
+			@Override
+			protected List<InformationFood> doInBackground(String... params) {
+				Map<String, String> arguments = new HashMap<String, String>();
+				arguments.put("type", "food");
+				arguments.put("uid", params[0]);
+				arguments.put("session", params[1]);
+				String jsonString = NetworkHelper.doHttpRequest(
+						NetworkHelper.STR_SERVER_URL, arguments);
+				if (jsonString != null) {
+
+				}
+
+				return null;
+			}
+
+			@Override
+			protected void onPostExecute(List<InformationFood> result) {
+
+				result = mFoods;
+
+				mFoodAdapter.setData(result);
+
+				// The list should now be shown.
+				if (isResumed()) {
+					setListShown(true);
+				} else {
+					setListShownNoAnimation(true);
+				}
+			}
+
 		}
 
 		class FoodAdapter extends BaseAdapter {
 
-			private List<InformationFood> mFoods = null;
+			private final LayoutInflater mInflater;
+			private List<InformationFood> mFoods = new ArrayList<InformationFood>();
 			private PictureManager mPictureManager = new PictureManager();
 			private OnPictureGetListener mOnPictureGetListener = new OnPictureGetListener() {
 				@Override
@@ -118,9 +225,20 @@ public class ConsumerMainActivity extends SherlockFragmentActivity {
 				}
 			};
 
-			FoodAdapter(List<InformationFood> foods) {
-				mFoods = foods;
+			FoodAdapter(Context context) {
+				mInflater = (LayoutInflater) context
+						.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 				mPictureManager.setOnPictureGetListener(mOnPictureGetListener);
+			}
+
+			void setData(List<InformationFood> foods) {
+				mFoods.clear();
+				if (foods != null) {
+					for (InformationFood f : foods) {
+						mFoods.add(f);
+					}
+				}
+				this.notifyDataSetChanged();
 			}
 
 			@Override
@@ -143,8 +261,8 @@ public class ConsumerMainActivity extends SherlockFragmentActivity {
 				InformationFood food = mFoods.get(position);
 
 				if (convertView == null) {
-					convertView = LayoutInflater.from(getActivity()).inflate(
-							R.layout.item_consumer_food, null);
+					convertView = mInflater.inflate(
+							R.layout.item_consumer_food, parent, false);
 				}
 
 				convertView.setTag(food);
