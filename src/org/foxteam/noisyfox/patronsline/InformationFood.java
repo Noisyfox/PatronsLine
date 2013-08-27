46f27bed5b431d184cf4b33af305f1d791f62f6b
noisyfox <timemanager.rick@gmail.com>
2013-08-27 15:05:58 +0800

    增加用于描述菜肴和店铺的类

---
+++ src/org/foxteam/noisyfox/patronsline/InformationFood.java
@@ -0,0 +1,14 @@
+package org.foxteam.noisyfox.patronsline;
+
+public class InformationFood {
+
+	String id;
+	String name;
+	float price;
+	boolean special;
+	String photo;
+	long likes;
+	long dislikes;
+	long comments;
+
+}
---
+++ src/org/foxteam/noisyfox/patronsline/InformationShop.java
@@ -0,0 +1,22 @@
+package org.foxteam.noisyfox.patronsline;
+
+import java.util.List;
+
+public class InformationShop {
+
+	boolean isDetailed;
+	String id;
+	String name;
+	boolean bookmark;
+	String photo;
+	float mark;
+	// detail
+	String address;
+	String introduction;
+	String phone_num;
+	String school;
+	float popularity;
+	float user_mark;
+	List<InformationFood> foods;
+
+}
