8a786336267065438218283a9c48d16a439e08a3
noisyfox <timemanager.rick@gmail.com>
2013-08-28 21:46:23 +0800

    完成用户登录

--- src/org/foxteam/noisyfox/patronsline/SessionManager.java
+++ src/org/foxteam/noisyfox/patronsline/SessionManager.java
@@ -1,8 +1,15 @@
 package org.foxteam.noisyfox.patronsline;
 
 import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 
 import android.graphics.Bitmap;
 import android.util.Log;
@@ -15,6 +22,14 @@ import android.util.Log;
  */
 public class SessionManager {
 	public static int ACTION_USER_REGISTER = 1;
+	public static int ACTION_USER_LOGIN = 2;
+
+	public static int ERROR_OK = 0;
+	public static int ERROR_IO_FAILURE = 1;
+	public static int ERROR_NETWORK_FAILURE = 2;
+	public static int ERROR_SERVER_FAILURE = 3;
+	public static int ERROR_USER_NAME_DUPLICATE = 4;
+	public static int ERROR_USER_LOGIN_FAILURE = 5;
 
 	InformationSession mSession;
 	OnSessionActionFinishedListener mOnSessionActionFinishedListener;
@@ -22,6 +37,14 @@ public class SessionManager {
 	public void setOnSessionActionFinishedListener(
 			OnSessionActionFinishedListener listener) {
 		mOnSessionActionFinishedListener = listener;
+	}
+
+	private void callListener(InformationSession session, int action,
+			int errCode) {
+		if (mOnSessionActionFinishedListener != null) {
+			mOnSessionActionFinishedListener.onSessionActionFinished(session,
+					action, errCode);
+		}
 	}
 
 	public void user_register(String name, String psw, int sex, int type,
@@ -37,26 +60,183 @@ public class SessionManager {
 		params.put("region", region);
 		ImageUploader.Image img = null;
 		try {
-			img = new ImageUploader.Image("avatar", "aaa", avatar);
+			img = new ImageUploader.Image("avatar", "headUpload_" + name,
+					avatar);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
-			if (mOnSessionActionFinishedListener != null) {
-				mOnSessionActionFinishedListener.onSessionActionFinished(null,
-						ACTION_USER_REGISTER, 1);
-				return;
-			}
+			callListener(null, ACTION_USER_REGISTER, ERROR_IO_FAILURE);
+			return;
 		}
 
-		String result = ImageUploader.post("", params.entrySet(),
-				new ImageUploader.Image[] { img });
+		String response = ImageUploader.post(NetworkHelper.STR_SERVER_URL,
+				params.entrySet(), new ImageUploader.Image[] { img });
 
-		Log.d("session", result);
+		if (response == null) {
+			callListener(null, ACTION_USER_REGISTER, ERROR_NETWORK_FAILURE);
+			return;
+		}
 
+		Log.d("session", response);
+
+		try {
+			JSONTokener jsonParser = new JSONTokener(response);
+			jsonParser.nextTo('{');
+			if (!jsonParser.more()) {
+				throw new JSONException("Failed to read return value.");
+			}
+			JSONObject jsonObj = (JSONObject) jsonParser.nextValue();
+			int result = jsonObj.getInt("result");
+			switch (result) {
+			case 1:// OK
+				break;
+			case -1:// 用户名重复
+				callListener(null, ACTION_USER_REGISTER,
+						ERROR_USER_NAME_DUPLICATE);
+				return;
+			case 0:// 服务器错误
+				callListener(null, ACTION_USER_REGISTER, ERROR_SERVER_FAILURE);
+				return;
+			}
+			String uid = jsonObj.getString("uid");
+			String session = jsonObj.getString("session");
+
+			InformationSession tmpSession = new InformationSession();
+			tmpSession.uid = uid;
+			tmpSession.session = session;
+
+			callListener(tmpSession, ACTION_USER_REGISTER, ERROR_OK);
+		} catch (JSONException e) {
+			e.printStackTrace();
+			callListener(null, ACTION_USER_REGISTER, ERROR_SERVER_FAILURE);
+			return;
+		}
+
+	}
+
+	private void loginUser(Map<Object, Object> params) {
+		String response = NetworkHelper.doHttpRequest(
+				NetworkHelper.STR_SERVER_URL, params.entrySet());
+
+		if (response == null) {
+			callListener(null, ACTION_USER_LOGIN, ERROR_NETWORK_FAILURE);
+			return;
+		}
+		Log.d("session", response);
+
+		try {
+			JSONTokener jsonParser = new JSONTokener(response);
+
+			jsonParser.nextTo('{');
+			if (!jsonParser.more()) {
+				throw new JSONException("Failed to read return value.");
+			}
+
+			JSONObject jsonObj = (JSONObject) jsonParser.nextValue();
+			int result = jsonObj.getInt("result");
+
+			switch (result) {
+			case 1:// OK
+				break;
+			case -1:// 登陆失败
+				callListener(null, ACTION_USER_LOGIN, ERROR_USER_LOGIN_FAILURE);
+				return;
+			case 0:// 服务器错误
+				callListener(null, ACTION_USER_LOGIN, ERROR_SERVER_FAILURE);
+				return;
+			}
+
+			// 登陆成功
+			String uid = jsonObj.getString("uid");
+			String session = jsonObj.getString("session");
+			int sex = jsonObj.getInt("sex");
+			int type = jsonObj.getInt("type");
+			String avatar = jsonObj.getString("avatar");
+			String school = jsonObj.getString("school");
+			String region = jsonObj.getString("region");
+
+			InformationSession tmpSession = new InformationSession();
+			InformationUser tmpUser = new InformationUser();
+			tmpSession.uid = uid;
+			tmpSession.session = session;
+			tmpSession.user = tmpUser;
+			tmpUser.sex = sex;
+			tmpUser.type = type;
+			tmpUser.avatar = avatar;
+			tmpUser.school = school;
+			tmpUser.region = region;
+			if (type == 1) {
+				String shopid = jsonObj.getString("shopid");
+				tmpUser.shopid = new ArrayList<String>();
+				tmpUser.shopid.add(shopid);
+			}
+
+			mSession = tmpSession;
+
+			PictureManager pictureManager = new PictureManager();
+			pictureManager.setOnPictureGetListener(new OnPictureGetListener() {
+				@Override
+				public void onPictureGet(String pid, Bitmap pic) {
+					if (pic != null && mSession != null
+							&& mSession.user != null
+							&& mSession.user.avatar == pid) {
+						mSession.user.avatarBitmap = pic;
+					}
+				}
+			});
+			pictureManager.getPicture(avatar);
+
+			callListener(mSession, ACTION_USER_LOGIN, ERROR_OK);
+
+		} catch (JSONException e) {
+			e.printStackTrace();
+			callListener(null, ACTION_USER_LOGIN, ERROR_SERVER_FAILURE);
+			return;
+		}
+	}
+
+	public void user_login(InformationSession userSession) {
+		Map<Object, Object> params = new HashMap<Object, Object>();
+		params.put("method", "user.login");
+		params.put("uid", userSession.uid);
+		params.put("session", userSession.session);
+
+		loginUser(params);
+	}
+
+	public void user_login(String userName, String psw) {
+		String psw_hash = hashString(psw, "SHA");
+		Map<Object, Object> params = new HashMap<Object, Object>();
+		params.put("method", "user.login");
+		params.put("name", userName);
+		params.put("password", psw_hash);
+
+		loginUser(params);
 	}
 
 	public static interface OnSessionActionFinishedListener {
 		public void onSessionActionFinished(InformationSession session,
 				int action, int errCode);
 	}
+
+	private static String hashString(String data, String algorithm) {
+		if (data == null)
+			return null;
+		try {
+			MessageDigest mdInst = MessageDigest.getInstance(algorithm);
+			byte btInput[] = data.getBytes();
+			mdInst.update(btInput);
+			byte md[] = mdInst.digest();
+			StringBuilder sb = new StringBuilder(64);
+			for (byte b : md) {
+				sb.append(String.format("%02X", b));
+			}
+
+			return sb.toString();
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+
 }
