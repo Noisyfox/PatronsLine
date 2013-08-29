64ebbd5fe5374979324846772e1875c467c7db73
noisyfox <timemanager.rick@gmail.com>
2013-08-30 01:38:28 +0800

    增加登陆后用户头像获取

--- src/org/foxteam/noisyfox/patronsline/ConsumerMainActivity.java
+++ src/org/foxteam/noisyfox/patronsline/ConsumerMainActivity.java
@@ -1,5 +1,9 @@
 package org.foxteam.noisyfox.patronsline;
 
+import org.foxteam.noisyfox.patronsline.PictureManager.OnPictureGetListener;
+
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.widget.TabHost;
@@ -11,6 +15,28 @@ public class ConsumerMainActivity extends SherlockFragmentActivity {
 	TabHost mTabHost;
 	ViewPager mViewPager;
 	TabsAdapter mTabsAdapter;
+
+	InformationSession mInformationSession = null;
+
+	PictureManager mAvatarPictureManager = new PictureManager();
+	OnPictureGetListener mAvatarOnPictureGetListener = new OnPictureGetListener() {
+		@Override
+		public void onPictureGet(String pid, Bitmap pic) {
+			InformationSession is = SessionManager.getSessionManager().mSession;
+			if (pic != null && is != null && is.user != null
+					&& is.user.avatar == pid) {
+				is.user.avatarBitmap = pic;
+				ActionBar actionBar = getSupportActionBar();
+				actionBar.setIcon(new BitmapDrawable(getResources(), pic));
+			}
+		}
+	};
+
+	@Override
+	protected void onDestroy() {
+		PictureManager.cacheSave();
+		super.onDestroy();
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +50,12 @@ public class ConsumerMainActivity extends SherlockFragmentActivity {
 
 		ActionBar actionBar = getSupportActionBar();
 		actionBar.setTitle("Noisyfox");
-		actionBar.setIcon(R.drawable.head);
 		actionBar.setSubtitle("欢迎回来");
+
+		mInformationSession = SessionManager.getSessionManager().mSession;
+		mAvatarPictureManager
+				.setOnPictureGetListener(mAvatarOnPictureGetListener);
+		mAvatarPictureManager.getPicture(mInformationSession.user.avatar);
 
 		mTabsAdapter = new TabsAdapter(this, mTabHost, mViewPager);
 		mTabsAdapter.addTab(
